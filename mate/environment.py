679c597d5f18b904bad055112ae1a48a5ed046f4
Tim E <tim.eckhardt@hotmail.com>
2024-09-23 18:19:13 +0200

    removed prints

--- mate/environment.py
+++ mate/environment.py
@@ -464,7 +464,6 @@ class MultiAgentTracking(gym.Env, EzPickle, metaclass=EnvMeta):
             ).astype(np.float64),
             dtype=np.float64,
         )
-        print(self.state_space.shape) # test
 
         self.obstacle_states = np.zeros(
             (self.num_obstacles, consts.OBSTACLE_STATE_DIM), dtype=np.float64
@@ -831,7 +830,6 @@ class MultiAgentTracking(gym.Env, EzPickle, metaclass=EnvMeta):
         self.target_message_queue.clear()
 
         self.episode_step = 0
-        print(len(self.joint_observation()))  # test
         return self.joint_observation(), dict()
 
     def send_messages(self, messages: Union[Message, Iterable[Message]]) -> None:
--- mate/wrappers/single_team.py
+++ mate/wrappers/single_team.py
@@ -88,7 +88,6 @@ def group_step(
     group_observe(agents, joint_observation, infos)
     group_communicate(env, agents)
     joint_action = group_act(agents, joint_observation, infos, deterministic=deterministic)
-    print(joint_action, "group step", joint_observation, "test")
 
     return joint_action
 
