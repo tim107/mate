f60658ead9b44750ef4ab90ebb694e7ad7ae4b83
Tim E <tim.eckhardt@hotmail.com>
2024-09-24 15:44:19 +0200

    group step test

--- mate/wrappers/single_team.py
+++ mate/wrappers/single_team.py
@@ -90,6 +90,7 @@ def group_step(
     group_observe(agents, joint_observation, infos)
     group_communicate(env, agents)
     joint_action = group_act(agents, joint_observation, infos, deterministic=deterministic)
+    print("joint actions", joint_action)
 
     return joint_action
 
