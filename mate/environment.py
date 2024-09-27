393f6e276e19eb7322e6485a2a578e5d3d8cce81
Tim E <tim.eckhardt@hotmail.com>
2024-09-27 16:50:29 +0200

    general reset test 8

--- mate/environment.py
+++ mate/environment.py
@@ -831,7 +831,7 @@ class MultiAgentTracking(gym.Env, EzPickle, metaclass=EnvMeta):
 
         self.episode_step = 0
         print(len(self.joint_observation()), "joint obs len")
-        return self.joint_observation()
+        return self.joint_observation(),
 
     def send_messages(self, messages: Union[Message, Iterable[Message]]) -> None:
         """Buffer the messages from an agent to others in the same team.
